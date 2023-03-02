be6620fa2e6e77bac9d685d0b0a361219e88d34a
kplei <98415753+kplei@users.noreply.github.com>
2023-03-02 09:19:07 -0500

    Create variables.tf

---
+++ AWS/redlure-worker/variables.tf
@@ -0,0 +1,35 @@
+variable "name" {
+  type        = string
+  description = "Name displayed in AWS"
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-0c7c4e3c6b4941f0f"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.small"
+}
+
+variable "security_group_id" {
+  type    = list(any)
+  default = ["<sg-1>", "<sg-2>"]
+}
+
+
+variable "key_name" {
+  default = "<public-key>"
+}
+
+variable "instance-count" {
+   type        = number
+   description = "number of instances to create"
+   default = 1
+}
+
+variable "console-ip" {
+   type    = string
+   default = "<redlure console IP or hostname>"
+}
